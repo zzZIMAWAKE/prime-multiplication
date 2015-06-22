d4d70bb759a9816e1cbb017bfd1bf17a1cfa1b5d
Nick <nickd64@googlemail.com>
2015-06-22 20:08:03 +0100

    Initial setup of project

---
+++ lib/prime-multiplication.rb
@@ -0,0 +1,1 @@
+require_relative 'initialize.rb'
---
+++ lib/prime-multiplication/prepare-prime.rb
@@ -0,0 +1,15 @@
+class PreparePrime < Numeric
+
+	def is_prime?
+		#check if a single number is prime
+	end
+	
+	def next_prime
+		#loop until we find a prime number here and return it to prepare_primes
+	end
+	
+	def prepare_primes(limit)
+	#prepare an array of prime numbers, equal to the limit
+		
+	end
+end
