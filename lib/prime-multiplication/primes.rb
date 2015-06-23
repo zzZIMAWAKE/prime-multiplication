7fec5474905733e8e08c4d3654e38eab3e39da6b
Nick <nickd64@googlemail.com>
2015-06-23 22:32:52 +0100

    Fixes to spacing

--- lib/prime-multiplication/primes.rb
+++ lib/prime-multiplication/primes.rb
@@ -1,53 +1,53 @@
 class Primes
 
-	def initialize(limit)
+    def initialize(limit)
         @ceiling
-		@count = 2
-		@limit = limit
-		@primeNumbers = []
-	end
+        @count = 2
+        @limit = limit
+        @primeNumbers = []
+    end
 
-	#populate array of primes up to the user
-	#specified limit
-	#@return array
-	def prepare_primes
-		j = 0
+    #populate array of primes up to the user
+    #specified limit
+    #@return array
+    def prepare_primes
+        j = 0
 
-		while j < @limit 
-			primeNumber = is_prime
-			if primeNumber
-				@primeNumbers.push(primeNumber)
-				j += 1
-			end
-			@count += 1
-		end
+        while j < @limit 
+            primeNumber = is_prime
+            if primeNumber
+            	@primeNumbers.push(primeNumber)
+            	j += 1
+            end
+            @count += 1
+        end
 
-		return @primeNumbers
-	end
+        return @primeNumbers
+    end
 
-	#check if a number is prime
-	#@return boolean
-	def is_prime
-		prime = true
-		i = 2
+    #check if a number is prime
+    #@return boolean
+    def is_prime
+        prime = true
+        i = 2
 
-		while i < @count
+        while i < @count
 
-			if @count % i === 0
-				prime = false
-			end
+            if @count % i === 0
+                prime = false
+            end
 
-			if @count === 2
-				prime = true
-			end
+            if @count === 2
+                prime = true
+            end
 
-			i += 1
-		end
+            i += 1
+        end
 
-		if prime
-			return @count
-		end
-	end
+        if prime
+            return @count
+        end
+    end
 
     #rough estimate of the range we will need
     #for the set limit
